968f746a72e8e252d4e5f4fd3677b9093f764d7b
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-01-21 19:31:56 +0100

    Done, 34 mins 14 secs

--- js/task-2.js
+++ js/task-2.js
@@ -1,63 +1,29 @@
-"use strict";
+class Storage {
+    #items = [];
 
-const getUsersWithFriend = (users, friendName) => {
-    return users.filter(user => user.friends.includes(friendName));
-};
+    constructor(items) {
+        this.#items = items;
+    }
 
-const allUsers = [
-    {
-        name: "Moore Hensley",
-        friends: ["Sharron Pace"],
-    },
-    {
-        name: "Sharlene Bush",
-        friends: ["Briana Decker", "Sharron Pace"],
-    },
-    {
-        name: "Ross Vazquez",
-        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    },
-    {
-        name: "Elma Head",
-        friends: ["Goldie Gentry", "Aisha Tran"],
-    },
-    {
-        name: "Carey Barr",
-        friends: ["Jordan Sampson", "Eddie Strong"],
-    },
-    {
-        name: "Blackburn Dotson",
-        friends: ["Jacklyn Lucas", "Linda Chapman"],
-    },
-    {
-        name: "Sheree Anthony",
-        friends: ["Goldie Gentry", "Briana Decker"],
-    },
-];
+    getItems() {
+        return this.#items;
+    }
 
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
 
-console.log(getUsersWithFriend(allUsers, "Briana Decker"));
-// [
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter((item) => {
+            return item !== itemToRemove;
+        })
+    }
+}
 
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// [
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 
-console.log(getUsersWithFriend(allUsers, "Adrian Cross")); // []
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
